261f9cb341b6626cb5c9116552027d2cd0d4a71b
vietphuongtran <55298689+vietphuongtran@users.noreply.github.com>
2020-04-02 11:49:59 -0400

    fixing get well soon list

---
+++ Migrations/202004012354554_update card design.cs
@@ -0,0 +1,16 @@
+﻿namespace ComicCorner.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatecarddesign : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
